1b3c3bc9d21c112de61881c6a4532aa09f46338b
Jeancarlos  Carvajal <jeancarloscarvajalavila@gmail.com>
2022-09-03 18:15:30 -0400

    Iniciando la Distribucion del Grid con el paquete

--- lib/src/pages/pinterest_page.dart
+++ lib/src/pages/pinterest_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class PinterestPage extends StatelessWidget {
    
@@ -8,7 +9,28 @@ class PinterestPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-         child: Text('PinterestPage'),
+         child: GridView.custom(
+          gridDelegate: SliverQuiltedGridDelegate(
+            crossAxisCount: 4,
+            mainAxisSpacing: 4,
+            crossAxisSpacing: 4,
+            repeatPattern: QuiltedGridRepeatPattern.inverted,
+            pattern: [
+              QuiltedGridTile(2, 2),
+              QuiltedGridTile(1, 1),
+              QuiltedGridTile(1, 1),
+              QuiltedGridTile(1, 2),
+            ],
+          ),
+          childrenDelegate: SliverChildBuilderDelegate( 
+            (context, index) {
+              return Container(
+                color: Colors.red,
+                child: Center(child: Text( 'Hola mundo' ))
+              );
+            },
+          ),
+        )
       ),
     );
   }
